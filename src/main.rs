77d0abb03cd80979a73581a99a86823a132d6b50
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2022-06-03 13:42:20 +0530

    headers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::fs;
 use std::io;
-mod convert;
+use std::env;
+use std::collections::HashSet;
+
+use serde_json::{json, Deserializer, Value};
+
 
 fn get_file_name () -> String {
 
@@ -11,31 +15,50 @@ fn get_file_name () -> String {
     file_name
 }
 
-fn convert2csv(data : String) -> String {
+// fn convert2csv(data : String) -> String {
 
-    // get all attribute headers
-    let headers = parse_headers(data);
+//     // get all attribute headers
+//     let headers = parse_headers(data);
 
-    // serialize their data
-    // for every json record, run parse_record
+//     // serialize their data
+//     // for every json record, run parse_record
 
-    // output csv formatted string
+//     // output csv formatted string
 
-}
+// }
 
 fn main() {
 
-    let filename = get_file_name();
+    let filename = env::args().nth(1).unwrap();
     
-    let json_content = fs::read_to_string(filename)
-        .expect("Cannot read the file.");
+    let mut json_content = {
+        let temp = fs::read_to_string(&filename).unwrap();
+        serde_json::from_str::<Value>(&temp).unwrap()
+    };
     
-    let csv_content = convert2csv(json_content);
+    
+    // get headers 
+    // let mut headers =  HashSet::new();
+
+    for i in json_content.as_array() {
+        //  println!("{:#?}",i);
+        for j in i {
+                // println!("{:#?}",j);
+                for key in j.as_array().unwrap() {
+                println!("{:#?}",key);
+                println!("ok")
+                // headers.insert(key);
+            }
+        }
+    }
+    
+    
 
     let len = filename.len()-4;
     let file_prefix = &filename[..len];
-    let file_prefix = format!("{}.csv",file_prefix);
+    let file_prefix = format!("{}csv",file_prefix);
 
-    fs::write(file_prefix, csv_content);
+    println!("{}", file_prefix);
+    // fs::write(file_prefix, csv_content);
 
 }
